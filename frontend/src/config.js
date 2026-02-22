ffec5ed7bf65a2910c4dfeedb61e087e60df5e71
knight0x01 <knight0x01@users.noreply.github.com>
2026-02-22 10:38:09 +0100

    feat: add network and contract configuration

---
+++ frontend/src/config.js
@@ -0,0 +1,7 @@
+import { StacksMainnet } from '@stacks/network';
+
+export const NETWORK = new StacksMainnet();
+export const CONTRACT_ADDRESS = 'SP1EPS1JHVHZ3MZRVY3381PDJJJ8PAFTDHMWAGK8P';
+export const MARKETPLACE_CONTRACT = 'nft-marketplace';
+export const AUCTION_CONTRACT = 'nft-auction';
+export const ESCROW_CONTRACT = 'nft-escrow';
